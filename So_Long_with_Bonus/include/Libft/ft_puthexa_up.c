86a1679fb457582fa2608961ddca4140d8977066
Arpit-42WOB <136806124+Arpit-42WOB@users.noreply.github.com>
2023-09-29 11:47:48 +0200

    Add files via upload

---
+++ So_Long_with_Bonus/include/Libft/ft_puthexa_up.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puthexa_up.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amehrotr <amehrotr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/02 16:27:24 by amehrotr          #+#    #+#             */
+/*   Updated: 2023/07/31 18:07:52 by amehrotr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_puthexachar(unsigned int num)
+{
+	if (num < 10)
+		return (ft_putchar(num + '0'));
+	else
+		return (ft_putchar(num + 55));
+}
+
+int	ft_puthexa_up(unsigned int num)
+{
+	int	i;
+	int	f;
+
+	i = 0;
+	if (num < 16)
+		return (ft_puthexachar(num));
+	else
+	{
+		f = ft_puthexa_up(num / 16);
+		if (f == -1)
+			return (-1);
+		i = i + f;
+		f = ft_puthexachar(num % 16);
+		if (f == -1)
+			return (-1);
+		i = i + f;
+	}
+	return (i);
+}
